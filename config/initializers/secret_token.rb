4eefbcfaa00622ece0df9f4b565d9b37427a3c7b
bristolplodder <matthew.cockburn@blueyonder.co.uk>
2013-06-16 15:46:15 +0100

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livetrainmap::Application.config.secret_token = '7da7e2a4aa1d8cea188e885b523921a881db5b6464760f193ba7236c910e6eef26f9e332eaa46bc7c236d72731bf602161248c90a88b355383f677ce0f9fc60b'
